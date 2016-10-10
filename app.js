7d94c024fef5a7a97ccbae5578ebdd2c130c4edb
Anthony <s3333113@student.rmit.edu.au>
2016-10-10 17:27:42 +1100

    changed to jade and added some dependencies

--- app.js
+++ app.js
@@ -5,23 +5,31 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var expressValidator = require("express-validator");
+var expressSession = require("express-session");
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+// environment plugin - make sure to add a .env in your dir!
+require("dotenv").config();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
 app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressSession({secret: "max", saveUninitialized: false, resave: false}));
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        key: process.env.TMDB_API_KEY,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        db: process.env.DB_DATABASE
+    });
 });
 
 module.exports = router;
