b0a8417d4b71a3fed7627085e0c73e7fa7452c04
Anthony <s3333113@student.rmit.edu.au>
2016-10-11 02:13:14 +1100

    added env test page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', {
+router.get("/", function(req, res, next) {
+    res.render("index", {title: "Home"});
+});
+
+router.get('/dotenvtest', function(req, res, next) {
+    res.render('dotenvtest', {
+        title: "dotenvtest",
         key: process.env.TMDB_API_KEY,
         host: process.env.DB_HOST,
         port: process.env.DB_PORT,
