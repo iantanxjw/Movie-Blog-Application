6b0f68769364ebba89bfb79d8bba3f00ed702c41
Anthony <s3333113@student.rmit.edu.au>
2016-10-14 16:26:58 +1100

    added some shitty crud routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var controllers = {
+    MovieController: require("../controllers/MovieController.js")
+};
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
@@ -9,5 +12,27 @@ router.get("/", function(req, res, next) {
 router.get('/admin', function(req, res, next) {
     res.render('admin', {title: "admin"});
 });
+
+// CRUD
+router.get("/movie/show", function(req, res) {
+    controllers.MovieController.show(req, res);
+});
+
+router.get("/movie/create", function(req, res) {
+    controllers.MovieController.create(req, res);
+});
+
+
+router.get("/movie/delete", function(req, res) {
+    controllers.MovieController.delete(req, res);
+});
+
+router.get("/movie/edit", function(req, res) {
+    controllers.MovieController.create(req, res);
+});
+
+router.get("/movie/apiupdate", function(req, res) {
+    controllers.MovieController.apiRequest(req, res);
+})
 
 module.exports = router;
