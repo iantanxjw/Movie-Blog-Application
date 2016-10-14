ee2f008b8445a8481645fcde48bf96f1f7f849e4
Anthony <s3333113@student.rmit.edu.au>
2016-10-14 22:38:45 +1100

    movie crud working

--- models/Movie.js
+++ models/Movie.js
@@ -1,2 +1,14 @@
-var Movie = mongoose.model("Movie", movieSchema);
-module.exports = Movie;
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+    id: {type: String, unique: true, required: true},
+    title: {type: String, required: true},
+    desc: String,
+    //genre: String,
+    voteAvg: Number,
+    poster: String,
+    bg: String
+});
+
+module.exports = mongoose.model("Movie", movieSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var controllers = {
-    MovieController: require("../controllers/MovieController.js")
+    MovieController: require("../controllers/MovieController"),
+    PostController: require("../controllers/PostController"),
+    CommentController: require("../controllers/CommentController")
 };
 
 /* GET home page. */
@@ -9,30 +11,92 @@ router.get("/", function(req, res, next) {
     res.render("index", {title: "Home"});
 });
 
+
+
 router.get('/admin', function(req, res, next) {
     res.render('admin', {title: "admin"});
 });
 
-// CRUD
-router.get("/movie/show", function(req, res) {
-    controllers.MovieController.show(req, res);
-});
-
-router.get("/movie/create", function(req, res) {
-    controllers.MovieController.create(req, res);
-});
-
-
-router.get("/movie/delete", function(req, res) {
-    controllers.MovieController.delete(req, res);
-});
-
-router.get("/movie/edit", function(req, res) {
-    controllers.MovieController.create(req, res);
-});
-
+// TEMPORARY
 router.get("/movie/apiupdate", function(req, res) {
     controllers.MovieController.apiRequest(req, res);
-})
+});
+
+
+/*################
+### MOVIE CRUD ###
+################*/
+router.route("/movie")
+    // get all movies
+    .get(function(req, res) {
+        controllers.MovieController.showAll(req, res);
+    })
+
+    // create a new movie
+    .post(function(req, res) {
+        controllers.MovieController.create(req, res);
+    });
+
+router.route("/movie/:mv_id")
+    // get specific movie
+    .get(function(req, res) {
+        controllers.MovieController.show(req, res);
+    })
+
+    // update a movie
+    .put(function(req, res) {
+        controllers.MovieController.update(req, res);
+    })
+
+    // delete a movie
+    .delete(function(req, res) {
+        controllers.MovieController.delete(req, res);
+    });
+
+
+/*####################
+### BLOG POST CRUD ###
+####################*/
+router.route("/post")
+    // get all posts
+    .get(function(req, res) {
+        controllers.PostController.showAll(req, res);
+    })
+
+    // create a new post
+    .post(function(req, res) {
+        controllers.PostController.create(req, res);
+    });
+
+router.route("/post/:post_id")
+    // get a specific post
+    .get(function(req, res) {
+        controllers.PostController.show(req, res);
+    })
+
+    // update post
+    .put(function(req, res) {
+        controllers.PostController.update(req, res);
+    })
+
+    // delete post
+    .delete(function(req, res) {
+        controllers.PostController.delete(req, res);
+    });
+
+/*##################
+### COMMENT CRUD ###
+##################*/
+router.route("/comment")
+    // create a comment
+    .post(function(req, res) {
+        controllers.CommentController.create(req, res);
+    });
+
+router.route("/comment/:comment_id")
+    // delete comment
+    .delete(function(req, res) {
+        controllers.CommentController.delete(req, res);
+    });
 
 module.exports = router;
